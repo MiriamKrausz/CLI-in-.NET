813235bc26c47368b1ebe2fed5328af8dc8e5dbb
miri <mk6764576@gmail.com>
2024-01-10 01:11:10 +0200

    bundle command

---
+++ Cli/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.CommandLine;
+var bundleCommand = new Command("bundle", "bundle code file's to a single file");
+var rootCommand = new RootCommand("Root command for file bundler CLI");
+bundleCommand.SetHandler(() => {
+    Console.WriteLine("bundle command");
+});    
+rootCommand.AddCommand(bundleCommand);
+rootCommand.InvokeAsync(args);
